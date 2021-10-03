e4cabd33c5a40626309814b44ef6924846b65bf6
Gichul Jaun <gichul@Gichuls-MacBook-Pro.local>
2021-10-03 18:04:14 -0400

    working on media queries

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,13 +5,17 @@ function Navbar() {
   return (
     <nav>
       <div className="wrapper">
-        <ul>
-          <li><img src={logo} alt="Fit Eternally logo on navbar" /></li>
-          <li><a href="#home">Home</a></li>
-          <li><a href="about">About</a></li>
-          <li><a href="process">Process</a></li>
-          <li><a href="shop">Shop</a></li>
-          <li><button>Sign up</button></li>
+        <ul className="nav-container">
+          <li className="logo-container">
+            <img src={logo} alt="Fit Eternally logo on navbar" />
+          </li>
+          <ul className="link-container">
+            <li><a href="#home">Home</a></li>
+            <li><a href="about">About</a></li>
+            <li><a href="process">Process</a></li>
+            <li><a href="shop">Shop</a></li>
+            <li><button>Sign up</button></li>
+          </ul>
         </ul>
       </div>
     </nav>
