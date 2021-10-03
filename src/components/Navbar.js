de4880d911bf732a4061dbe1b1cddc89614a2cba
Gichul Jaun <gichul@Gichuls-MacBook-Pro.local>
2021-10-03 15:50:21 -0400

    initializing production branch

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,14 @@ import '../styles/navbar.scss'
 
 function Navbar() {
   return (
-    <nav>
+    <nav id="home">
       <div className="wrapper">
         <ul>
           <li><img src={logo} alt="Fit Eternally logo on navbar" /></li>
           <li><a href="#home">Home</a></li>
-          <li><a href="about">About</a></li>
-          <li><a href="process">Process</a></li>
-          <li><a href="shop">Shop</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#process">Process</a></li>
+          <li><a href="#shop">Shop</a></li>
           <li><button>Sign up</button></li>
         </ul>
       </div>
