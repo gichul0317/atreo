e12d909891baaa4f0b0a0ab118ff6a7ef480b83b
Gichul Jaun <gichul@Gichuls-MacBook-Pro.local>
2021-10-03 20:30:57 -0400

    fixed internal link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,9 +11,9 @@ function Navbar() {
           </li>
           <ul className="link-container">
             <li><a href="#home">Home</a></li>
-            <li><a href="about">About</a></li>
-            <li><a href="process">Process</a></li>
-            <li><a href="shop">Shop</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#process">Process</a></li>
+            <li><a href="#shop">Shop</a></li>
             <li><button>Sign up</button></li>
           </ul>
         </ul>
